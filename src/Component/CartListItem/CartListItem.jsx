16bfb77209cb8bf2338bc242fb006030d6a9200a
BismaAsif <bismaasif274@gmail.com>
2021-06-22 00:48:16 +0500

    cart design is done

--- src/Component/CartListItem/CartListItem.jsx
+++ src/Component/CartListItem/CartListItem.jsx
@@ -1,4 +1,6 @@
 import { connect } from "react-redux";
+import './CartListItem.css';
+import img5 from './../../Images/shoes.jpg';
 import { addproduct,removeproduct,deleteproduct } from "../../Redux/Cart/CartActions";
 
 
@@ -7,8 +9,23 @@ const CartListItem=({ addproduct,removeproduct,deleteproduct,...item})=>{
     var {cost,title,quantity,coverPhoto,id}=item;
     return(
         <>
- <h3>{title}-{cost}</h3>
-        <h1><button onClick={()=> addproduct(item)}>+</button >  {quantity}  <button onClick={()=> removeproduct(id)}>-</button></h1>
+        <div className="carbox">
+            <div className="displaypic">
+<img className="fit" src={img5}/>
+            </div>
+ <h3 className="displaytext">{title}</h3>
+ <p className="left">Lorem ipsum lorem ipsum lorem ipsum</p>
+ 
+ <div className="qbox">
+    <p className="dq">{quantity}</p>
+    
+ <div className="dq1"><p>+</p><p> -</p> </div>
+ </div>
+ <div  className="displaycost">
+     <h2>$-{cost}</h2>
+ </div>
+        {/* <h1><button onClick={()=> addproduct(item)}>+</button >  {quantity}  <button onClick={()=> removeproduct(id)}>-</button></h1> */}
+        </div>
         </>
 
     )
