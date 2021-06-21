b7461b319b3c6bc9d341cb17681ce8736985dea0
BismaAsif <bismaasif274@gmail.com>
2021-06-22 00:48:28 +0500

    cart design is done

--- src/Component/CartList/CartList.jsx
+++ src/Component/CartList/CartList.jsx
@@ -1,12 +1,14 @@
 import {connect} from "react-redux";
 import CartListItem from "../CartListItem/CartListItem";
+import './CartList.css'
 
 const CartList=({itemlist})=>{
     console.log(itemlist)
     return(
         <>
-
+<div className="box">
 {itemlist.map((item)=><CartListItem key={item.id} {...item}/>)}
+</div>
         </>
         )
 }
