e41e2a80bd2ff14260658c8a982ce8df011912e2
BismaAsif <bismaasif274@gmail.com>
2021-06-22 15:12:01 +0500

    cart updated

--- src/Component/Cart/Cart.jsx
+++ src/Component/Cart/Cart.jsx
@@ -1,18 +1,38 @@
 import CartList from "../CartList/CartList";
 import { OrderGenerator } from "../../Redux/Order/OrderActions";
 import { connect } from "react-redux";
+import CartTotal from "../../Utility/CartTotal";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import { Link } from "react-router-dom";
+import './Cart.css';
 
 
-const Cart=({OrderGenerator})=>{
+const Cart=({OrderGenerator,total})=>{
     return(
         <>
-        <h3>CART</h3>
+        <Navbar/>
+        <div className="area">
+        <Link to="/product">
+        <p className="return"><ArrowBackIosIcon/> Return to All Products</p></Link><p className="viewlist"><FavoriteBorderOutlinedIcon/> View wishlist</p>
+        <h3 className="ysc">Your Shopping Cart <ShoppingCartOutlinedIcon fontSize="large" className="iconshop"/></h3>
         <CartList/>
-<button onClick={OrderGenerator} >Checkout</button>
+        <div className="cartbox">
+        <p className="stotal">SubTotal</p>  <h3 className="totalcost">${total}</h3>
+        </div>
+<button className="checkoutbtn" onClick={OrderGenerator} >Checkout</button>
+</div>
+<Footer/>
         </>
         )
 }
 var actions={
     OrderGenerator
 }
-export default connect(null,actions)(Cart);
+var mapState=(state)=>({
+    total: CartTotal(state.cart)
+})
+export default connect(mapState,actions)(Cart);
--- src/Component/CartListItem/CartListItem.jsx
+++ src/Component/CartListItem/CartListItem.jsx
@@ -1,11 +1,14 @@
 import { connect } from "react-redux";
 import './CartListItem.css';
 import img5 from './../../Images/shoes.jpg';
+import CartTotal from "../../Utility/CartTotal";
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { addproduct,removeproduct,deleteproduct } from "../../Redux/Cart/CartActions";
 
 
 
-const CartListItem=({ addproduct,removeproduct,deleteproduct,...item})=>{
+const CartListItem=({ addproduct,removeproduct,deleteproduct,total,...item})=>{
     var {cost,title,quantity,coverPhoto,id}=item;
     return(
         <>
@@ -19,13 +22,14 @@ const CartListItem=({ addproduct,removeproduct,deleteproduct,...item})=>{
  <div className="qbox">
     <p className="dq">{quantity}</p>
     
- <div className="dq1"><p>+</p><p> -</p> </div>
+ <div className="dq1"><p><ExpandLessIcon onClick={()=>addproduct(item)} fontSize="small"/></p><p> <ExpandMoreIcon onClick={()=> removeproduct(id)} fontSize="small"/> </p></div>
  </div>
  <div  className="displaycost">
-     <h2>$-{cost}</h2>
+    <h2>$-{cost}</h2>
  </div>
         {/* <h1><button onClick={()=> addproduct(item)}>+</button >  {quantity}  <button onClick={()=> removeproduct(id)}>-</button></h1> */}
         </div>
+        
         </>
 
     )
@@ -34,4 +38,5 @@ const CartListItem=({ addproduct,removeproduct,deleteproduct,...item})=>{
 var actions={
     addproduct,removeproduct,deleteproduct
 }
+
 export default connect(null,actions)(CartListItem);
